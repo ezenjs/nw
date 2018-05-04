1c76372a01dad51553c6308a5fcfc882088ae430
ezen.ai1023 <ezen.ai1023@gmail.com>
2018-05-04 17:50:12 +0900

    web - login and join

--- server.py
+++ server.py
@@ -1,18 +1,9 @@
 # -*- coding: utf8 -*-
 # 소켓 라이브러리 로딩
 import socket
-# 서버 정보
-info = ("127.0.0.1", 9999)
-# 소켓 생성
-s = socket.socket()
-# 9999번 포트 바인딩
-s.bind(info)
-# 바인딩 포트 리스닝
-s.listen(5)
-while True:
-    # 접속요청 승인
-    conn, address = s.accept()
-    print "[+] new connection from %s(%d)" % (address[0], address[1])
+import threading
+
+def handler(conn, adrress):
     while True:
         try:
             # 클라이언트가 전송한 데이터 수신
@@ -27,3 +18,19 @@ while True:
         print "address %s send data: %s" % ( address[0], data)
         # 수신 데이터를 클라이언트에 전송
         conn.send(data)
+
+# 서버 정보
+# * 아래의 ip주소를 대신할수있는 다른것은 * <- 와일드카드
+info = ("0.0.0.0", 9999)
+# 소켓 생성
+s = socket.socket()
+# 9999번 포트 바인딩
+s.bind(info)
+# 바인딩 포트 리스닝
+s.listen(5)
+while True:
+    # 접속요청 승인
+    conn, address = s.accept()
+    print "[+] new connection from %s(%d)" % (address[0], address[1])
+    th = threading.Thread(target=handler, args=(conn, address))
+    th.start()
