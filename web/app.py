5fc12056a25e742d9b3cea491a8f3c80d641009c
ezen.ai1023 <ezen.ai1023@gmail.com>
2018-05-14 17:50:27 +0900

    log_in_out_page

--- web/app.py
+++ web/app.py
@@ -1,24 +1,67 @@
-from flask import Flask, render_template, request
+# coding:utf-8
+from flask import Flask, render_template, request, g, redirect, session, escape
 import hashlib
+import sqlite3
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
-users = {}
+app.secret_key = '5rhg5ry6ythgtg5hf65489069845ytythf'
+
+def get_db():
+    db = getattr(g, '_database' , None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False, modify=False):
+    cur = get_db().execute(query, args)
+    if modify:
+        try:
+            get_db().commit()
+            cur.close()
+        except:
+            return False
+        return True
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.route("/logout")
+def logout():
+    session.pop('id', None)
+    return redirect('/login')
 
 @app.route("/")
 def hello():
+    if 'id' in session:
+        return 'Logged in as %s <a href="/logout">logout</a>' %escape(session['id'])
     return render_template("login.html")
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    id = request.form['id']
-    pw = request.form['pw']
-    if id in users:
-        if users[id] == hashlib.sha1(pw).hexdigest():
-            return "login ok"
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = hashlib.sha1(request.form['pw'].strip()).hexdigest()
+        sql = "select * from user where id='%s' and password='%s'" % (id, pw)
+        if query_db(sql, one=True):
+            # 로그인이 성공한 경우
+            session['id'] = id
+            return redirect("/")
         else:
-            return "login fail"
-    else:
-        return "login fail"
+            # 로그인이 실패한 경우
+            return "<script>alert('join fail'); history.back(-1);</script>"
+
+    if 'id' in session:
+        return redirect("/")
+
+    return render_template("login.html")
 
 @app.route("/name")
 def name():
@@ -27,13 +70,24 @@ def name():
 @app.route("/join", methods=['GET', 'POST'])
 def join():
     if request.method == 'POST':
-        id = request.form['id']
-        pw = request.form['pw']
-        if id not in users:
-            users[id] = hashlib.sha1(pw).hexdigest()
-        else:
-            return "duplicate!!!"
-        return "join ok"
+        id = request.form['id'].strip()
+        pw = hashlib.sha1(request.form['pw'].strip()).hexdigest()
+
+        sql = "select * from user where id='%s'" % id
+        if query_db(sql, one=True):
+            return "<script>alert('join fail'); history.back(-1);</script>"
+
+        sql = "insert into user(id, password) values('%s', '%s')" % (id, pw)
+        query_db(sql, modify=True)
+       # if id not in users:
+       # users[id] = hashlib.sha1(pw).hexdigest()
+       # else:
+       #     return "duplicate!!!"
+        return redirect("/login")
+
+    if 'id' in session:
+        return redirect("/")
+
     return render_template("join.html")
 
 @app.route("/add")
